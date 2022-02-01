c65fe153d22bc91744d4b93dcdabd662ae20d0c8
ArturPromon <artur.shovkunenko@gmail.com>
2022-02-01 19:44:48 +0200

    ifelse

---
+++ src/com/company/Main2.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args){
+        int peopleage;
+        int constage;
+        constage = 62;
+        Scanner num = new Scanner(System.in);
+        System.out.println("How old are you?");
+        peopleage = num.nextInt();
+
+        if(peopleage > constage || peopleage == constage){
+            System.out.print("Your pension is 300 euro per month");
+        }else{
+            int dif = constage - peopleage;
+            if(dif < 5){
+                System.out.print("You need to work just " + dif + " years more for pension");
+            }else{
+                System.out.print("You are so young for pension!!!");
+            }
+
+        }
+    }
+}
---
+++ src/com/company/Main3.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 1;
+        b = 5;
+
+        if(a>2 && a<11 || b >= 6 && b<14){
+            System.out.print("true");
+        }else{
+            System.out.print("false");
+        }
+    }
+}
