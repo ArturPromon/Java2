353e98501ae792415ce73cb474e037871b679305
ArturPromon <artur.shovkunenko@gmail.com>
2022-02-03 19:51:07 +0200

    praktika

---
+++ src/com/company/Main4.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int first = 5;
+        System.out.println("Enter first number: ");
+        first = scn.nextInt();
+
+        if(first ==5){
+            System.out.println("number is 5");
+        }else{
+            System.out.println("number is not 5");
+        }
+
+        switch(first){
+            case 5:
+                System.out.println("number is 5");
+                break;
+            case 6:
+                System.out.println("number is 6");
+                break;
+            default:
+                System.out.println("Number is " + first);
+        }
+    }
+}
